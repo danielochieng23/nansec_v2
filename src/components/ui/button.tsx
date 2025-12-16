cfd913d52aa1937313e80ccc5f90fa554faaeaad
Isaac <isaactony303@gmail.com>
2025-12-16 09:35:26 +0530

    Please see the change in button code its was throwing error

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -31,11 +31,7 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants>;
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, ...props }, ref) => {
